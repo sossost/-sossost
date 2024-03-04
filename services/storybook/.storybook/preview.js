8b24d8ac2a556a66a3636735543546fb51b65d78
jang_yoonsu <shagrat8546@gmail.com>
2024-03-04 23:54:18 +0900

    feat : preview 세팅 추가

--- services/storybook/.storybook/preview.js
+++ services/storybook/.storybook/preview.js
@@ -1,3 +1,6 @@
+import "@soaf/themes/themes.css";
+import "./style.css";
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -8,7 +11,44 @@ const preview = {
         date: /Date$/i,
       },
     },
+    rootAttributesTooltip: true,
+    rootAttributes: [
+      {
+        root: "body",
+        attribute: "class",
+        defaultState: {
+          name: "light",
+          value: "theme-light",
+        },
+        states: [
+          {
+            name: "dark",
+            value: "theme-dark",
+          },
+        ],
+      },
+    ],
   },
 };
+
+const initTheme = () => {
+  const isDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  if (isDarkTheme) {
+    document.body.classList.add("theme-dark");
+  }
+
+  const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
+
+  mediaQueryList.addEventListener("change", (e) => {
+    if (e.matches) {
+      document.body.classList.add("theme-dark");
+    } else {
+      document.body.classList.remove("theme-dark");
+    }
+  });
+};
+
+initTheme();
 
 export default preview;
