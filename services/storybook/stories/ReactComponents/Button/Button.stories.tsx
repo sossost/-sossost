551bd66b97cdfdc7dbaad53bb4d68123efd2d445
jang_yoonsu <shagrat8546@gmail.com>
2024-03-06 23:05:30 +0900

    feat : Add Button Story

---
+++ services/storybook/stories/ReactComponents/Button/Button.stories.tsx
@@ -0,0 +1,37 @@
+import "@soaf/react-components-button/style.css";
+import { Button as _Button } from "@soaf/react-components-button";
+import { vars } from "@soaf/themes";
+
+export default {
+  title: "React Components/Button",
+  component: _Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    size: {
+      options: ["xs", "sm", "md", "lg"],
+      control: "select",
+    },
+    color: {
+      options: Object.keys(vars.colors.$scale),
+      control: "select",
+    },
+    variant: {
+      options: ["solid", "outline", "ghost"],
+      control: "select",
+    },
+  },
+};
+
+export const ButtonStory = {
+  args: {
+    size: "lg",
+    children: "Button",
+    variant: "outline",
+    isDisabled: false,
+    isLoading: false,
+    leftIcon: "😀",
+  },
+};
