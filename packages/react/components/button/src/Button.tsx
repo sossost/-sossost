673694f2b917eb916fce6a24844b2cda2ef22baa
jang_yoonsu <shagrat8546@gmail.com>
2024-03-06 23:03:54 +0900

    feat : Add Button packages

---
+++ packages/react/components/button/src/Button.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import { ButtonProps } from "./types";
+import { clsx } from "clsx";
+import { assignInlineVars } from "@vanilla-extract/dynamic";
+import {
+  buttonStyle,
+  activeColorVariant,
+  enableColorVariant,
+  hoverColorVariant,
+} from "./style.css";
+import { vars } from "@soaf/themes";
+
+const Button = (props: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
+  const {
+    variant = "solid",
+    size = "md",
+    color = "gray",
+
+    isDisabled = false,
+    children,
+    style,
+  } = props;
+
+  const enableColor = vars.colors.$scale[color][500];
+  const hoverColor =
+    variant === "solid"
+      ? vars.colors.$scale[color][600]
+      : vars.colors.$scale[color][50];
+  const activeColor =
+    variant === "solid"
+      ? vars.colors.$scale[color][700]
+      : vars.colors.$scale[color][100];
+
+  const disabled = isDisabled;
+
+  return (
+    <button
+      {...props}
+      ref={ref}
+      className={clsx([
+        buttonStyle({
+          size,
+          variant,
+        }),
+      ])}
+      disabled={disabled}
+      style={{
+        ...assignInlineVars({
+          [enableColorVariant]: enableColor,
+          [hoverColorVariant]: hoverColor,
+          [activeColorVariant]: activeColor,
+        }),
+        ...style,
+      }}
+    >
+      {children}
+    </button>
+  );
+};
+
+const _Button = React.forwardRef(Button);
+export { _Button as Button };
