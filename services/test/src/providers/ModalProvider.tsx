d61c90fc3ca70567e312075dae8e704959b00609
jang_yoonsu <shagrat8546@gmail.com>
2024-03-20 23:42:53 +0900

    refactor : Refactor ModalProvider

--- services/test/src/providers/ModalProvider.tsx
+++ services/test/src/providers/ModalProvider.tsx
@@ -1,73 +1,78 @@
 "use client";
 
-import { createContext, useCallback, useState } from "react";
+import { ReactNode, createContext, useCallback, useState } from "react";
 import ModalContainer from "../components/ModalContainer";
 
 type ModalContextType = {
-  push: (key: string, Component: any, props: any) => Promise<any>;
+  push: (
+    key: string,
+    Component: React.ComponentType<any>,
+    props: any,
+  ) => Promise<any>;
   pop: () => void;
   clear: () => void;
-  top: any;
-  modalInfos: any[];
+  top: ModalInfo | undefined;
+};
+
+type ModalInfo = {
+  key: string;
+  Component: React.ComponentType<any>;
+  props: any;
+  resolve: (value: any) => void;
+  reject: (reason: any) => void;
 };
 
 export const ModalContext = createContext<ModalContextType | null>(null);
 
-export const ModalProvider = ({ children }) => {
-  const [modalInfos, setModalInfos] = useState<any[]>([]);
-  const [flag, setFlag] = useState(0);
+export const ModalProvider = ({ children }: { children: ReactNode }) => {
+  const [modalInfos, setModalInfos] = useState<ModalInfo[]>([]);
 
-  const flush = useCallback(() => {
-    setFlag((prev) => prev + 1);
-  }, []);
+  const handlePromise = (
+    key: string,
+    resolver: (value: unknown) => void,
+    value: unknown,
+  ) => {
+    resolver(value);
+    setModalInfos((infos) => infos.filter((info) => info.key !== key));
+  };
 
-  const handlePromise = useCallback(
-    (key, resolver, value) => {
-      resolver(value);
-      setModalInfos((infos) => infos.filter((info) => info.key !== key));
-      flush();
-    },
-    [flush],
-  );
+  const push = async (
+    key: string,
+    Component: React.ComponentType<any>,
+    props: any,
+  ) => {
+    return new Promise((resolve, reject) => {
+      setModalInfos((infos) => [
+        ...infos,
+        {
+          key,
+          Component,
+          props,
+          resolve: (value) => handlePromise(key, resolve, value),
+          reject: (reason) => handlePromise(key, reject, reason),
+        },
+      ]);
+    });
+  };
 
-  const push = useCallback(
-    async (key, Component, props) => {
-      return new Promise((resolve, reject) => {
-        setModalInfos((infos) => [
-          ...infos,
-          {
-            key,
-            Component,
-            props,
-            resolve: (value) => handlePromise(key, resolve, value),
-            reject: (reason) => handlePromise(key, reject, reason),
-          },
-        ]);
-        flush();
-      });
-    },
-    [handlePromise, flush],
-  );
-
-  const pop = useCallback(() => {
+  const pop = () => {
     const topInfo = modalInfos[modalInfos.length - 1];
     if (!topInfo) return;
 
     topInfo.reject(`Close modal: ${topInfo.key}`);
     setModalInfos((infos) => infos.slice(0, infos.length - 1));
-    flush();
-  }, [modalInfos, flush]);
+  };
 
-  const clear = useCallback(() => {
+  const clear = () => {
     while (modalInfos.length > 0) {
       pop();
     }
-  }, [modalInfos, pop]);
+  };
 
   const top = modalInfos[modalInfos.length - 1];
 
   return (
-    <ModalContext.Provider value={{ push, pop, clear, top, modalInfos }}>
+    <ModalContext.Provider value={{ push, pop, clear, top }}>
       {children}
       <ModalContainer />
     </ModalContext.Provider>
