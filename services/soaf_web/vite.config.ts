f602705063db1e8505c508142c8a60bea4d73fb7
jang_yoonsu <shagrat8546@gmail.com>
2024-03-24 23:29:37 +0900

    chore : Add stackflow libs

--- services/soaf_web/vite.config.ts
+++ services/soaf_web/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@shared", replacement: "/src/components" },
+      { find: "@features", replacement: "/src/styles" },
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@libs", replacement: "/src/libs" },
+    ],
+  },
+});
