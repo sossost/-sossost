05192bcf143270f62fa59493e738a35335d47171
jang_yoonsu <shagrat8546@gmail.com>
2024-04-07 11:43:35 +0900

    refactor : Refactor immutable deepcopy func

--- services/js_test/immutable/__test__/immutable.test.js
+++ services/js_test/immutable/__test__/immutable.test.js
@@ -1,18 +1,69 @@
-import { deepCopy1, deepCopy2 } from "../src";
+import { deepCopy } from "../src";
 
 describe("deep copy test", () => {
   const obj1 = { a: 1, b: "a", c: "가" };
   const arr1 = [1, 2, obj1];
-  const obj2 = { a: 1, b: obj1, c: arr1 };
-  const obj3 = [1, obj2, arr1, obj1];
+  const date1 = new Date();
+  const regExp1 = new RegExp("a");
+  const func1 = () => {};
+  const map1 = new Map();
+  map1.set("a", 1);
+  const set1 = new Set();
+  set1.add(1);
 
-  const objArrForTest = [obj1, obj2, obj3];
+  const obj2 = {
+    a: 1,
+    b: "a",
+    c: "가",
+    d: obj1,
+    e: arr1,
+    f: date1,
+    g: regExp1,
+    h: func1,
+    i: map1,
+    j: set1,
+  };
+  const obj3 = {
+    a: 1,
+    b: "a",
+    c: "가",
+    d: obj1,
+    e: arr1,
+    f: date1,
+    g: regExp1,
+    h: func1,
+    i: map1,
+    j: set1,
+    k: obj2,
+  };
+  const obj4 = {
+    a: 1,
+    b: "a",
+    c: "가",
+    d: obj1,
+    e: arr1,
+    f: date1,
+    g: regExp1,
+    h: func1,
+    i: map1,
+    j: set1,
+    k: obj3,
+  };
+
+  const objArrForTest = [
+    obj1,
+    arr1,
+    date1,
+    regExp1,
+    func1,
+    map1,
+    set1,
+    obj2,
+    obj3,
+    obj4,
+  ];
 
   test.each(objArrForTest)("deepCopy1 function test", (obj) => {
-    expect(deepCopy1(obj)).toEqual(obj);
-  });
-
-  test.each(objArrForTest)("deepCopy2 function test", (obj) => {
-    expect(deepCopy2(obj)).toEqual(obj);
+    expect(deepCopy(obj)).toEqual(obj);
   });
 });
--- services/js_test/immutable/src/index.js
+++ services/js_test/immutable/src/index.js
@@ -1,8 +1,13 @@
-export function deepCopy1(object) {
-  const coppiedObj = Array.isArray(object) ? [] : {};
+export function getObjectType(object) {
+  return Object.prototype.toString.call(object).slice(8, -1);
+}
+
+export function deepCopyObject(object) {
+  const coppiedObj = {};
+
   for (const key in object) {
     if (typeof object[key] === "object") {
-      coppiedObj[key] = deepCopy1(object[key]);
+      coppiedObj[key] = deepCopy(object[key]);
     } else {
       coppiedObj[key] = object[key];
     }
@@ -11,6 +16,39 @@ export function deepCopy1(object) {
   return coppiedObj;
 }
 
-export function deepCopy2(object) {
-  return JSON.parse(JSON.stringify(object));
+export function deepCopyArray(object) {
+  const coppiedObj = [];
+
+  for (let i = 0; i < object.length; i++) {
+    if (typeof object[i] === "object") {
+      coppiedObj[i] = deepCopy(object[i]);
+    } else {
+      coppiedObj[i] = object[i];
+    }
+  }
+
+  return coppiedObj;
+}
+
+export function deepCopy(object) {
+  const objectType = getObjectType(object);
+
+  switch (objectType) {
+    case "Object":
+      return deepCopyObject(object);
+    case "Array":
+      return deepCopyArray(object);
+    case "Date":
+      return new Date(object);
+    case "RegExp":
+      return new RegExp(object);
+    case "Function":
+      return object;
+    case "Map":
+      return new Map(object);
+    case "Set":
+      return new Set(object);
+    default:
+      return object;
+  }
 }
