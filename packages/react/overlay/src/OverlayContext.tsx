f71585b4dd4ac95eab6dc86690226298807dd625
sossost <shagrat8546@gmail.com>
2024-07-10 19:51:30 +0900

    feat: Add OverlayContext component for managing overlays

---
+++ packages/react/overlay/src/OverlayContext.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+import { useCreateOverlayContainer } from './useCreateOverlayContainer';
+import { OverlayRenderer } from './OverlayRenderer';
+
+export const OverlayContext = ({ children }: { children: ReactNode }) => {
+  useCreateOverlayContainer();
+
+  return (
+    <>
+      {children}
+      <OverlayRenderer />
+    </>
+  );
+};
